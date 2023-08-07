072ac58725c4b5cfa4aae35237fa4bdbc7929c1f
Repith <119136592+Repith@users.noreply.github.com>
2023-08-07 23:54:52 +0200

    Minor UI changes (date to Dashbord recent orders)

--- ecommerce-admin/components/recent-orders.tsx
+++ ecommerce-admin/components/recent-orders.tsx
@@ -1,9 +1,10 @@
-import { Order } from "@prisma/client";
+import { Order, OrderItem } from "@prisma/client";
 import Link from "next/link";
 import { Badge } from "./ui/badge";
+import { formatDistanceToNow } from "date-fns";
 
 interface RecentOrderProps {
-  data: Order[];
+  data: Order[] | { orderItems: OrderItem[] };
 }
 
 const RecentOrders: React.FC<RecentOrderProps> = ({ data }) => {
@@ -12,19 +13,28 @@ const RecentOrders: React.FC<RecentOrderProps> = ({ data }) => {
       {data.map((order, index) => (
         <div key={index} className="p-2">
           <Link href={`/${order.storeId}/orders/${order.id}`}>
-            <div className="flex justify-between p-2 border-2">
-              {order.orderItems.map((item, itemIndex) => (
-                <span key={itemIndex}>{item.product.name}, </span>
-              ))}
-              {order.isPaid ? (
-                order.isSent ? (
-                  <Badge variant="success">Sent</Badge>
+            <div className="grid p-2 border-2 grid-col-3">
+              <div className="col-span-1">
+                {order.orderItems.map((item, itemIndex) => (
+                  <div key={itemIndex}>
+                    {`${item.product.name} x ${item.quantity} `}
+                  </div>
+                ))}
+              </div>
+              <div className="col-span-2">
+                {formatDistanceToNow(new Date(order.createdAt))} ago
+              </div>
+              <div className="col-span-3">
+                {order.isPaid ? (
+                  order.isSent ? (
+                    <Badge variant="success">Sent</Badge>
+                  ) : (
+                    <Badge variant="warning">Pending</Badge>
+                  )
                 ) : (
-                  <Badge variant="warning">Pending</Badge>
-                )
-              ) : (
-                <Badge variant="destructive">Processing</Badge>
-              )}
+                  <Badge variant="destructive">Processing</Badge>
+                )}
+              </div>
             </div>
           </Link>
         </div>
